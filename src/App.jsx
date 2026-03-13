9f306609b0a37cf922a863214d1ee93c8715e38d
Quantumscripts-ai <quantumscripts24@gmail.com>
2026-03-13 19:41:00 +0530

    Add Vercel config, 404 page, and deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AboutPage from './pages/AboutPage'
 import ServicesPage from './pages/ServicesPage'
 import BlogPage from './pages/BlogPage'
 import ContactPage from './pages/ContactPage'
+import NotFoundPage from './pages/NotFoundPage'
 import ScrollToTop from './components/ScrollToTop'
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
       <Footer />
